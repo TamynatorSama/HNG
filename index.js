6dc73049823e5c8132162ea87b4ed78071730248
kolawole tamilore <63677298+tamy-k@users.noreply.github.com>
2022-10-28 07:11:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const port = process.env.PORT || 3000
 const server = http.createServer((req,res)=>{
+    res.setHeader('Access-Control-Allow-origin','*')
+    res.setHeader('Access-Control-Request-Method','*')
+    res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET')
+    res.setHeader('Access-Control-Allow-Headers','*')
     res.writeHead(200,{
         "content-Type": "application/json"
     })
